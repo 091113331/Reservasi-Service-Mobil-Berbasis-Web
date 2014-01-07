e779f421c9442639277e87a3ee309ca6c9befab0
091112557 <martin_erikson_siburian@ymail.com>
2014-01-07 22:46:54 +0700

    Create style.php

---
+++ style.php
@@ -0,0 +1,18 @@
+/* 
+    Document   : style
+    Created on : 16 Nov 13, 8:13:07
+    Author     : bpm
+    Description:
+        Purpose of the stylesheet follows.
+*/
+
+/* 
+   TODO customize this sample style
+   Syntax recommendation http://www.w3.org/TR/REC-CSS2/
+*/
+
+root { 
+    display: block;
+}
+#main{width:900px;position:relative; margin:0 auto; padding:0;}
+#header {width:100%;height:100px;margin:0 auto; padding :0; border:1px solid;}
