25447ec8b24a4272bc4e37087fa4fd6564728bb4
091112557 <martin_erikson_siburian@ymail.com>
2014-01-07 22:00:45 +0700

    Create koneksi.php

---
+++ config/koneksi.php
@@ -0,0 +1,22 @@
+<?php
+
+$my['host']	= "localhost";
+$my['user']	= "root";
+$my['pass']	= "";
+$my['dbs']	= "dbpkm";
+
+$koneksi	= mysql_connect($my['host'],
+							$my['user'],
+							$my['pass']);
+if (! $koneksi) {
+  echo "Gagal koneksi boss..!";
+  mysql_error();
+}
+mysql_select_db($my['dbs'])
+	 or die ("Database nggak ada tuh".mysql_error());
+
+
+
+
+
+?>
